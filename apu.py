1c0bb8106e4c8b41d5dc6a81e6356c015ca9ed94
kouki888 <yioyo5039@gmail.com>
2026-04-20 23:23:47 +0800

    Update apu.py

--- apu.py
+++ apu.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAKE7etexSgsiI41vjHnXg3UcUVNJoSEhE")
+genai.configure(api_key="AIzaSyCeHCeUDsrOHP0Fziqyr-2vqH7GR1W73Bk")
 
 for m in genai.list_models():
     print(m.name, m.supported_generation_methods)
